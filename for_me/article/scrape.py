d4cc0d04fdb19ce957f74cfbc583662f0dc3e727
Rahul Duggal <ibrahul24@gmail.com>
2017-12-08 10:17:34 -0500

    working on classifying articles

--- for_me/article/scrape.py
+++ for_me/article/scrape.py
@@ -1,21 +1,18 @@
-import pandas as pd 
+# import pandas as pd 
 
-hn_df = pd.DataFrame(data=None, columns=['type', 'title', 'url'])
+# hn_df = pd.DataFrame(data=None, columns=['type', 'title', 'url'])
 
-for num in range(0,6):
-	file_name = 'output%s' % num + '.csv'
-	df = pd.read_csv(file_name)
-	df = df[(df.type == 'story')]
-	df = df.drop(['time', 'score', 'id', 'by', 'text'], axis=1)
-	hn_df = hn_df.append(df, ignore_index=True)	
+# for num in range(0,6):
+# 	file_name = 'output%s' % num + '.csv'
+# 	df = pd.read_csv(file_name)
+# 	df = df[(df.type == 'story')]
+# 	df = df.drop(['time', 'score', 'id', 'by', 'text'], axis=1)
+# 	hn_df = hn_df.append(df, ignore_index=True)	
 
-hn_df = hn_df[hn_df.title.notnull() & hn_df.url.notnull()]
-print(hn_df)
+# hn_df = hn_df[hn_df.title.notnull() & hn_df.url.notnull()]
+# print(hn_df)
 
 # Classifying each of the articles with regex
-
-
-
 
 # Python	python|pandas
 # Mobile	mobile|android|iphone|phone
@@ -35,3 +32,8 @@ print(hn_df)
 # Data Science	data scien|big data|data vi|data ?set|data analy|machine learning|pandas|ggplot2
 # Science	bio|drug|researcher|genomic|physics|scienti|spacex|\\bmoon\\b|nasa|\\bastro|\\bmars\\b
 # Math	math|geome|cryptograph|algebra|calculus
+
+
+
+
+
